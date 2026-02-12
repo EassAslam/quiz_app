481454ec56a234f3a4817afe5f1394a719852283
EassAslam <eass1815@gmail.com>
2026-02-12 11:35:25 -0500

    created questions data model and list of questions

---
+++ lib/questions.dart
@@ -0,0 +1,57 @@
+import 'package:quiz_app/quiz_question.dart';
+const questions = [
+  QuizQuestion(
+    'How did Edwar Elric lose his arm in FMA?',
+    [
+      'Putting his brothers soul in a suit of armor',
+      'in a duel',
+      'he never had any arms',
+    ]
+  ),
+  QuizQuestion(
+  'What is the capital city of France?',
+  [
+    'Paris',
+    'Lyon',
+    'Marseille',
+    'Bordeaux',
+  ],
+),
+QuizQuestion(
+  'Which planet is known as the Red Planet?',
+  [
+    'Mars',
+    'Venus',
+    'Jupiter',
+    'Saturn',
+  ],
+),
+QuizQuestion(
+  'How many elements are listed in the periodic table?',
+  [
+    '118',
+    '112',
+    '120',
+    '108',
+  ],
+),
+QuizQuestion(
+  'Which ocean is the largest on Earth?',
+  [
+    'Pacific Ocean',
+    'Atlantic Ocean',
+    'Indian Ocean',
+    'Arctic Ocean',
+  ],
+),
+QuizQuestion(
+  'Who painted the "Mona Lisa"?',
+  [
+    'Leonardo da Vinci',
+    'Vincent van Gogh',
+    'Pablo Picasso',
+    'Claude Monet',
+  ],
+),
+
+];
