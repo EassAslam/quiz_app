a1f578cfbc4019a126477b73e02a1fac1374485c
EassAslam <eass1815@gmail.com>
2026-02-19 12:03:16 -0500

    did some basic styling on the answer buttons

--- lib/answer_button.dart
+++ lib/answer_button.dart
@@ -17,7 +17,7 @@ class AnswerButton extends StatelessWidget {
       onPressed: onTap,
       style: ElevatedButton.styleFrom(
         padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 40),
-        backgroundColor: const Color.fromARGB(255, 33, 1, 95),
+        backgroundColor: const Color.fromARGB(255, 21, 1, 61),
         foregroundColor: Colors.white,
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(40),
